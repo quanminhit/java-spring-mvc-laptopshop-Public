f3c9e951decdea410d6b42e84540ee2139e11315
Quan Nguyen <louisnguyen296@gmail.com>
2024-08-28 19:25:43 +0700

    mvc

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,25 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/minhquan")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "eric.html";
+    }
+}
